9946bc43cf8f2584a694769b7a93fee3f20620b1
uriya <ooria47@gmail.com>
2022-10-07 01:56:31 +0300

    CORS support

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3001;
 const app: Express = express();
 
 app.use((req, res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://warehouse-staff.herokuapp.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
     if ('OPTIONS' == req.method) return res.sendStatus(204);

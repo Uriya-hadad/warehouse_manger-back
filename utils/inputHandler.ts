371a486f33a31203c0d0b2613217885eeedf5548
Shlomo Shtraus <shloymi987@gmail.com>
2023-04-24 16:02:03 +0300

    Change quantity validation error

--- utils/inputHandler.ts
+++ utils/inputHandler.ts
@@ -1,5 +1,7 @@
-export function checkIfQuantityIsValid(quantity:number,numberOfItemsSold:number){
-    if (quantity - numberOfItemsSold < 0 ||
+export function checkIfQuantityIsValid(quantity: number, numberOfItemsSold: number) {
+    if (quantity === 0) {
+        throw new Error("Product is out of stock");
+    } else if (quantity < numberOfItemsSold ||
         numberOfItemsSold < 1)
-        throw new Error(`invalid quantity: ${numberOfItemsSold}`)
+        throw new Error(`invalid quantity: ${numberOfItemsSold} \nAvailable: ${quantity}`);
 }
